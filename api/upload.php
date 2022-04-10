69110bc6d15d0c58b326fb82142cc66d5f5c180f
SumimotoTakara <96.sumimoto@gmail.com>
2022-04-10 16:44:50 +0900

    同名ファイルのアップロードはファイル名を変更するよう修正

--- api/upload.php
+++ api/upload.php
@@ -1,6 +1,8 @@
 <?php
+require_once('./functions.php');
 
 
+const STORAGE_PATH = '../storage/';
 
 // $_FILESはinputである以上必ず渡ってくるので、nameプロパティに文字列が入っているかどうかで判別
 try {
@@ -9,7 +11,12 @@ try {
     $filename = $_FILES['file']['name'];
 
     // move_uploaded_file()の引数ように格納先のパスを変数へ代入。
-    $upload_path = '../storage/' . $filename;
+    $upload_path = STORAGE_PATH . $filename;
+
+    // 同名ファイルの処理
+    if (file_exists($upload_path)) {
+      $upload_path = change_same_filename($filename, $upload_path, STORAGE_PATH);
+    }
 
     // ファイル移動を実行。move_uploaded_fileの結果(フラグ)を受け取る
     $result_flag = move_uploaded_file($_FILES['file']['tmp_name'], $upload_path);
@@ -26,7 +33,7 @@ try {
     throw new Exception('POSTされた内容に問題があります');
   }
 } catch (Exception $e) {
-  $result = array('code' => 400, 'message' => 'erroe', 'description' => $e->getMessage());
+  $result = array('code' => 400, 'message' => 'error', 'description' => $e->getMessage());
   echo json_encode($result, JSON_UNESCAPED_UNICODE);
   exit;
 }
